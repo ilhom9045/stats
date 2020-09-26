8b9c33910c4f01f1bd6abdefdc2dac79a4beccb4
ilhom <fozilov.7459045@gmail.com>
2020-09-26 21:46:25 +0500

    stats

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ilhom9045/stats
 
 go 1.15
+
+require github.com/ilhom9045/bank v1.0.0
--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -1,2 +1,30 @@
 package stats
 
+import "github.com/ilhom9045/bank/pkg/types"
+
+func Avg(payments []types.Payment) types.Money {
+	money := types.Money(0)
+	count := types.Money(len(payments))
+
+	for _, value := range payments {
+		money += value.Amount
+	}
+
+	money = money / count
+	return money
+}
+
+func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
+	money := types.Money(0)
+
+	for _, value := range payments {
+
+		if value.Category == category {
+
+			money += value.Amount
+
+		}
+	}
+
+	return money
+}
