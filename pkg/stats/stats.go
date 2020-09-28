82ff80deda2cc127220b7e20131e79ea6206055b
ilhom <fozilov.7459045@gmail.com>
2020-09-28 22:18:57 +0500

    func Periods.. was created

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -1,6 +1,8 @@
 package stats
 
-import "github.com/ilhom9045/bank/v2/pkg/types"
+import (
+	"github.com/ilhom9045/bank/v2/pkg/types"
+)
 
 func Avg(payments []types.Payment) types.Money {
 	money := types.Money(0)
@@ -17,16 +19,16 @@ func Avg(payments []types.Payment) types.Money {
 //FilterByCategory
 func FilterByCategory(payments []types.Payment, category types.Category) []types.Payment {
 	var filtered []types.Payment
-		for _, payment := range payments {
-			if payment.Category == category {
-				filtered = append(filtered, payment)
-			}
+	for _, payment := range payments {
+		if payment.Category == category {
+			filtered = append(filtered, payment)
 		}
-		return filtered
+	}
+	return filtered
 }
 
 //CategoriesAvg, которая считает среднюю сумму платежа по каждой категории
-func CategoriesAvg(payments []types.Payment)  map[types.Category]types.Money {
+func CategoriesAvg(payments []types.Payment) map[types.Category]types.Money {
 	n := make(map[types.Category]types.Money)
 	// categories := map[types.Category]types.Money{}
 	for _, payment := range payments {
@@ -34,7 +36,27 @@ func CategoriesAvg(payments []types.Payment)  map[types.Category]types.Money {
 			continue
 		}
 		filtered := FilterByCategory(payments, payment.Category)
-		n[payment.Category]=Avg(filtered)
+		n[payment.Category] = Avg(filtered)
 	}
-	return n 
+	return n
+}
+
+func PeriodsDynamic(
+	first map[types.Category]types.Money,
+	second map[types.Category]types.Money,
+) map[types.Category]types.Money {
+
+	periods := map[types.Category]types.Money{}
+
+	if len(first) > len(second) {
+		for value := range first {
+			periods[value] = second[value] - first[value]
+		}
+		return periods
+	}
+	for value := range second {
+		periods[value] = second[value] - first[value]
+	}
+	return periods
+
 }
